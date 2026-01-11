7a0ac50ad3bb7fcffd0309e8c380c858014dde38
wingardium1leviosa <romanchuksolomiia@icloud.com>
2026-01-11 20:55:36 +0100

    hometask3_2 modified

--- hometask3_2.py
+++ hometask3_2.py
@@ -2,9 +2,11 @@
 
 
 # example 1
-my_list = [5, 7, 10, 11]
-a = my_list.pop(3)
-print(my_list)
-my_list.insert(0, a)
-print(my_list)
+my_list = [1, 6, 7, 5]
+if len(my_list) >= 2:
 
+    a = my_list.pop()
+    my_list.insert(0, a)
+    print(my_list)
+else :
+    print(my_list)
